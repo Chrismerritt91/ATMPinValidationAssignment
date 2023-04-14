dfc6276ddc57fc99e5a7b1dec4fb7edf0eb3f4f6
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-14 11:46:02 -0500

    create pin validation for assignment

--- Program.cs
+++ Program.cs
@@ -1,3 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class DebitCard
+{
 
+    private string _pin;
+
+    public string Pin
+    {
+        get
+        {
+            return _pin;
+        }
+        set
+        {
+            if (value == null || value == "")
+            {
+                Console.WriteLine("Input Cannot Be Blank");
+            }
+            else
+            {
+                bool b = int.TryParse(value, out int i);
+                if (b == false)
+                {
+                    _pin = "Invalid";
+                }
+                else
+                {
+                    if (value.Length == 4 || value.Length == 6)
+                    {
+                        _pin = value;
+                    }
+                    else
+                    {
+                        _pin = "Invalid";
+                    }
+                }
+            }
+
+
+        }
+    }
+
+}
+
+class program
+{
+
+    static void Main()
+    {
+
+        DebitCard d = new DebitCard();
+
+        d.Pin = "1234";
+        Console.WriteLine(d.Pin);
+        d.Pin = "1234ef";
+        Console.WriteLine(d.Pin);
+        d.Pin = "123456";
+        Console.WriteLine(d.Pin);
+        d.Pin = "0";
+        Console.WriteLine(d.Pin);
+        d.Pin = "a4";
+        Console.WriteLine(d.Pin);
+        d.Pin = null;
+        Console.WriteLine(d.Pin);
+        Console.ReadKey();
+
+    }
+
+}
